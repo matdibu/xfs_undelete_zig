62316a75f32b1ae38e0678010166a8fed12ecf91
Matei Dibu <contact@mateidibu.dev>
2024-05-23 14:05:26 +0300

    add hello world exe

--- build.zig
+++ build.zig
@@ -4,35 +4,12 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const libxfs_undelete = b.addSharedLibrary(.{
-        .name = "xfs_undelete",
+    const exe = b.addExecutable(.{
+        .name = "hello",
+        .root_source_file = b.path("bin/hello.zig"),
         .target = target,
         .optimize = optimize,
     });
-    libxfs_undelete.linkLibCpp();
-    libxfs_undelete.linkSystemLibrary("spdlog");
-    libxfs_undelete.linkSystemLibrary("handle");
 
-    const cflags = [_][]const u8{
-        "-Wall",
-        "-Wextra",
-        "-pedantic",
-        "-std=c++20",
-    };
-
-    libxfs_undelete.addCSourceFiles(
-        .{
-            .files = &.{
-                "lib/linux_file.cpp",
-                "lib/xfs_exceptions.cpp",
-                "lib/xfs_extent.cpp",
-                "lib/xfs_inode.cpp",
-                "lib/xfs_inode_entry.cpp",
-                "lib/xfs_parser.cpp",
-            },
-            .flags = &cflags,
-        },
-    );
-
-    b.installArtifact(libxfs_undelete);
+    b.installArtifact(exe);
 }
