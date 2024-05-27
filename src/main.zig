cdf02e00a3c1d8613a7bfce734224ad15f767f16
Matei Dibu <contact@mateidibu.dev>
2024-05-27 12:33:26 +0300

    use std.log

--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,5 @@
 const std = @import("std");
 
 pub fn main() !void {
-    const stdout = std.io.getStdOut();
-    var bw = std.io.bufferedWriter(stdout.writer());
-    const w = bw.writer();
-    try w.print("hello!", .{});
-    try bw.flush();
+    std.log.info("hello!", .{});
 }
