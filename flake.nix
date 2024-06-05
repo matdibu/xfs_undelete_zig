daee690571b404282ec03835b1d5f6cad7bea094
Matei Dibu <contact@mateidibu.dev>
2024-06-05 11:16:33 +0300

    flake: update

--- flake.nix
+++ flake.nix
@@ -1,18 +1,42 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    zig = {
+      url = "github:mitchellh/zig-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-compat.follows = "flake-compat";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+    zls = {
+      url = "github:zigtools/zls";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        zig-overlay.follows = "zig";
+        flake-utils.follows = "flake-utils";
+      };
+    };
     treefmt-nix = {
       url = "github:numtide/treefmt-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    flake-compat.url = "github:edolstra/flake-compat";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.systems.follows = "systems";
+    };
+    systems.url = "github:nix-systems/default";
   };
 
   outputs =
     {
       self,
       nixpkgs,
-      systems,
+      zig,
+      zls,
       treefmt-nix,
+      systems,
       ...
     }:
     let
@@ -32,11 +56,14 @@
       devShells = eachSystem (pkgs: {
         default = pkgs.mkShellNoCC {
           packages = with pkgs; [
-            zig
+            zig.packages.${pkgs.system}.master
+            zls.packages.${pkgs.system}.zls
             spdlog.dev
             pkg-config
-            libxfs
             libuuid.dev
+            libxfs
+            util-linux
+            util-linux.dev
             python3
           ];
         };
