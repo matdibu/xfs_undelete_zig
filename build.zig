cca149eff8b9a2434bb2071216ce20f904ed3ff2
Matei Dibu <contact@mateidibu.dev>
2024-06-05 11:21:54 +0300

    cleanup build.zig

--- build.zig
+++ build.zig
@@ -4,9 +4,6 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const zigcli_dep = b.dependency("zig-cli", .{ .target = target });
-    const zigcli_mod = zigcli_dep.module("zig-cli");
-
     const xfs_undelete = b.addExecutable(.{
         .name = "xfs_undelete",
         .root_source_file = b.path("src/main.zig"),
@@ -14,16 +11,21 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
         .link_libc = true,
     });
+
+    const zigcli_dep = b.dependency("zig-cli", .{ .target = target });
+    const zigcli_mod = zigcli_dep.module("zig-cli");
     xfs_undelete.root_module.addImport("zig-cli", zigcli_mod);
 
     xfs_undelete.addSystemIncludePath(.{
         .cwd_relative = "/nix/store/23v80anf3q5zip8ldlps7hzijnj0gx8w-xfsprogs-6.6.0-dev/include",
     });
+
+    xfs_undelete.linkSystemLibrary("uuid");
+    // the target header is at $UTIL_LINUX/include/uuid/uuid.h
+    // "pkg-config uuid" returns the "include/uuid" subdir,
+    // but xfsprogs-dev wants to include "uuid/uuid.h"
     xfs_undelete.addSystemIncludePath(.{
-        .cwd_relative = "/nix/store/sw3a1cypmpgh8gvlhhxby0wl9f80wg53-util-linux-minimal-2.40.1-dev/include",
-    });
-    xfs_undelete.addSystemIncludePath(.{
-        .cwd_relative = "/nix/store/2hmd2c81sv9qpdh49xvcyvr6m1iahrs5-linux-6.9.2-dev/lib/modules/6.9.2/source/include",
+        .cwd_relative = "/nix/store/h45bgvgr7ljv8qx5jmrd9vw1ll7df29l-util-linux-minimal-2.39.3-dev/include",
     });
 
     b.installArtifact(xfs_undelete);
@@ -31,8 +33,6 @@ pub fn build(b: *std.Build) void {
     const run_demo = b.addRunArtifact(xfs_undelete);
     const run_step = b.step("run", "Run the application");
     run_step.dependOn(&run_demo.step);
-    // This allows the user to pass arguments to the application in the build
-    // command itself, like this: `zig build run -- arg1 arg2 etc`
     if (b.args) |args| {
         run_demo.addArgs(args);
     }
