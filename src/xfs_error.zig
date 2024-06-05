d90f9e5167dd8eabe615d61375a0d590bea6698c
Matei Dibu <contact@mateidibu.dev>
2024-06-05 11:02:48 +0300

    add new xfs_error's

--- src/xfs_error.zig
+++ src/xfs_error.zig
@@ -1,8 +1,12 @@
 pub const xfs_error = error{
+    no_device,
     sb_magic,
     sb_version,
     agf_magic,
     agi_magic,
     no_0_start_offset,
     btree_block_magic,
+    xfs_ext_unwritten,
+    xfs_ext_zeroed,
+    xfs_ext_beyond_sb,
 };
